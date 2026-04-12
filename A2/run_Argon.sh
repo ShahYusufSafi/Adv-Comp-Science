189f586058d519dea32ca5883e96cd898af1fbae
Shah Yusuf Safi <yusufsafi277@gmail.com>
2026-04-12 21:36:49 +0000

    A2 Update

--- A2/run_Argon.sh
+++ A2/run_Argon.sh
@@ -14,11 +14,13 @@ gcc -o amclj ../amclj.c ../getval.c -lm
 N=256
 RHO=0.8445
 TEMP=0.6995
-DISP=0.1
 DR=0.02
+
+DISP=0.15
 NTSKIP=10
-NTPRINT=100
-NTJOB=5000
+NTPRINT=10
+NTJOB=50000
+
 
 echo "Generating initial configuration..."
 printf "${N}\n${RHO}\n${TEMP}\n${DISP}\n${DR}\n${NTSKIP}\n${NTPRINT}\n${NTJOB}\nmclj_in.dat\n" | ./lmclj
@@ -34,11 +36,11 @@ printf "${N}\n${RHO}\n${TEMP}\n${DISP}\n${DR}\n${NTSKIP}\n${NTPRINT}\n${NTJOB}\n
 # -------------------------
 printf "mclj_out.dat\n${N}\n${RHO}\n${TEMP}\n${DISP}\n${DR}\n10\n100\n50000\nmclj_in.dat\n" | ./zmclj
 
-
+echo f"=========================="
 echo "Running simulation..."
 ./mclj | tee log.dat
 
 echo "Analyzing results..."
-echo "y" | ../amclj | tee amclj.logprintf "mclj_out.dat\ny\n" | ../amclj | tee amclj.log
-
+#echo "y" | ../amclj | tee amclj.logprintf "mclj_out.dat\ny\n" | ../amclj | tee amclj.log
+printf "%s\n%s\n" "mclj_out.dat" "y" | ./amclj | tee amclj.log
 echo "Done! Results in log.dat and amclj.log"
